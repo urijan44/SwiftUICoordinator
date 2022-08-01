70e4e85cc2302ee2b363cd31761dc0a5bd2b64d3
leehs81 <leehs81@brandi.co.kr>
2022-08-01 11:13:55 +0900

    Initial Commit

---
+++ SwiftUICoordinator/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  SwiftUICoordinator
+//
+//  Created by leehs81 on 2022/08/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUICoordinator/SwiftUICoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICoordinatorApp.swift
+//  SwiftUICoordinator
+//
+//  Created by leehs81 on 2022/08/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
